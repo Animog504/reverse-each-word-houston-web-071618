f74e3b89494e84a6821005267ab6e1595aebac20
Scott Ungchusri <animoogle504@gmail.com>
2018-07-07 21:22:40 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
 def reverse_each_word(string)
-  #newString = ""
+  newString = ""
   arrayString = string.split(" ")
   arrayString.each do |words|
        words.reverse!
-       print "#{words} "
+       newString += "#{words} "
   end
- 
+  return newString
 end
 
 reverse_each_word("Hello there, and how are you?")
